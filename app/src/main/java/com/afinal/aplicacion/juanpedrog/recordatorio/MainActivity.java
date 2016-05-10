e3bc7cab30ad04f6c58b6db98c744d4c3f27b2a6
juanpedrog <juanpedrogilllanos@hotmail.com>
2016-05-10 12:02:54 -0500

    Initial commit

---
+++ app/src/main/java/com/afinal/aplicacion/juanpedrog/recordatorio/MainActivity.java
@@ -0,0 +1,13 @@
+package com.afinal.aplicacion.juanpedrog.recordatorio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
