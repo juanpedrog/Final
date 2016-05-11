1379f671e48569521021665262ad7da2759b31dc
juanpedrog <juanpedrogilllanos@hotmail.com>
2016-05-10 19:07:22 -0500

    Programacion del boton de nuevo registro, creacion de la activity "Registro"

---
+++ app/src/main/java/com/afinal/aplicacion/juanpedrog/recordatorio/Registro.java
@@ -0,0 +1,13 @@
+package com.afinal.aplicacion.juanpedrog.recordatorio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
